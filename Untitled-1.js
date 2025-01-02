d78726034cf6ac66910b727a3f7cb7fad034b38f
JL-GALE <jorge.galem@gmail.com>
2025-01-01 19:06:44 -0500

    Declaracion de variables y tipos da datos que se usan en js

---
+++ Untitled-1.js
@@ -0,0 +1,35 @@
+let numero1 = 10;
+let numero2 = 20;
+let numero3 = 30;
+
+console.log(numero1);
+console.log(numero2);
+console.log(numero3);
+
+//Se puede usar $, _
+let $variable1 = 1;
+let _variable2 = 2;
+let $miNumero = 100;
+
+console.log($variable1);
+console.log(_variable2);
+console.log($miNumero);
+
+/*
+Number -> almacena valores numericos (10, 100, -10, 100, 10.000, 12.434, -129.232)
+String -> almacena valores de tipo cadena de caractees("Hola mundo", 'Hola mundo', `Hello world`)
+Boolean -> almecena valores de tipo boalno (True, False)
+Null -> Sigfica ausencia de referencia de un objeto
+Undefined -> Significa ausencia de valor
+*/
+
+let number = 10;
+console.log(number);
+let string = 'Hello world';
+console.log(string);
+let boalno = false;
+console.log(boalno);
+let nulo = null;
+console.log(nulo);
+let vacio = undefined;
+console.log(vacio);
