3f43a96346410007f3f4c2591cc377e9674130c5
JL-Gale <jorge.galem@gmail.com>
2025-01-08 18:46:26 -0500

    Se practico los operadopres de asignacion

---
+++ Leccion 2/OperaderesDeAsignacion.js
@@ -0,0 +1,33 @@
+
+
+//Operadores de asignacion
+
+let numero = 10;
+console.log(numero);
+
+numero = 20;
+console.log(numero);
+
+//Operador +=
+numero += 50;
+console.log(numero);
+
+//Operador -=
+numero -=20;
+console.log(numero);
+
+//Operador *=
+numero *= 2;
+console.log(numero);
+
+//Operador /=
+numero /= 2;
+console.log(numero);
+
+//Operador %=
+numero %= 3;
+console.log(numero);
+
+//Operador **/
+numero **=4;
+console.log(numero);
