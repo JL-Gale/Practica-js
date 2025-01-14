cd9bbd742764ad571efd22c046cf0f1e1a77e911
JL-Gale <jorge.galem@gmail.com>
2025-01-14 10:00:31 -0500

    Practica de los operadores de comparacion

---
+++ Leccion 2/OperadoresDeComparacion.js
@@ -0,0 +1,33 @@
+//Opdores de comparacion 
+
+let a = 20;
+let b = "20";
+
+//Igualdad ==
+console.log(a == b);
+
+//Igualdad Eestricta ===
+console.log(a === b);
+
+
+//Desigualdad !=
+console.log(a != b);
+
+//Desigualdad estricta !==
+console.log(a !== b);
+
+//Mayor que >
+a = 21;
+console.log(a > b);
+
+//Mayor o igual que >=
+a = 20;
+console.log(a >= b);
+
+//Menor que <
+a = 20;
+console.log(a < b);
+
+//Menor o igual <=
+a = 19;
+console.log(a <= b);
