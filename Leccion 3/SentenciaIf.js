835ff9ad8ed642cf7e0f533ae5f301f074a9edd1
JL-Gale <jorge.galem@gmail.com>
2025-01-15 13:04:58 -0500

    Practica de la sentencia if

---
+++ Leccion 3/SentenciaIf.js
@@ -0,0 +1,12 @@
+//Sentencia if 
+
+let numero = 10; 
+
+if(numero < 10) {
+    console.log("La sentencia es trueeee");
+}
+
+if(numero <= 10) {
+    console.log("La Sentencia es truee");
+    console.log(`El numro es ${numero}`);
+}
