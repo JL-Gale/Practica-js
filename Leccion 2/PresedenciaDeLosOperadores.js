f0012fd21e1112f14e4c6d8deb5235fef6e60620
JL-Gale <jorge.galem@gmail.com>
2025-01-15 12:47:47 -0500

    Practica de la presedencia de los operadores

---
+++ Leccion 2/PresedenciaDeLosOperadores.js
@@ -0,0 +1,18 @@
+//Procedencia Operadores
+
+//1. Parentesis y Corchetes
+//2. Operadores Unarios, como ++, --; !
+//3. Aritmeticos *, /, %
+//4. Aritmeticos + y -
+//5. Relacionales <, <=, > y >=
+//6. Iguales == y !=
+//7. Logicos && y ||
+//8. Asignacion =, =+, -=, *=, etc
+
+//Una expresion se reviza de izquierda a derecha 
+let resul = 12 / 3 + 2 * 3 - 1;
+//1) 12 / 3 = 4
+//2) 2 * 3 = 6
+//3) 4 + 6 = 10
+//4) 10 - 1 = 9
+console.log(resul);
