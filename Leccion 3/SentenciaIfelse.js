67a8e5af69a2078561b517b38c476e909bd8bfd8
JL-Gale <jorge.galem@gmail.com>
2025-01-15 13:48:49 -0500

    Practica de la sentencia if else

---
+++ Leccion 3/SentenciaIfelse.js
@@ -0,0 +1,9 @@
+//Sentencia if else
+
+let numero = 22;
+
+if (numero <= 100 || numero >= 200) {
+    console.log(`El numero es <= 100 o >= 200: ${numero}`);
+} else {
+    console.log(`El numero no es <= 100 o >= 200: ${numero}`);
+}
