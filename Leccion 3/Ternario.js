60ea03a00eac24b865d7b92acf3d5621f166a96c
JL-Gale <jorge.galem@gmail.com>
2025-01-16 09:18:37 -0500

    Practica con ternarios

---
+++ Leccion 3/Ternario.js
@@ -0,0 +1,10 @@
+let a = "Jorge";
+let b = "jorgE"
+
+console.log(`a: ${a}, b: ${b}`);
+
+let result = (a.toLowerCase() === b.toLowerCase()) ? "Son iguales" : "No son iguales";
+let test = (a.toLowerCase() === b.toLowerCase()) ? true : false;
+
+console.log(test);
+console.log(result);
