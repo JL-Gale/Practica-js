2cfad400f48086767fb901801c6655a4d6626aa5
JL-Gale <jorge.galem@gmail.com>
2025-02-23 12:39:43 -0500

    Se Practicaron las difernetes formas de guardar datos, se practico algunos tipos de datos y la concatenacion de variables

---
+++ 02 - variables/main.js
@@ -0,0 +1,25 @@
+//Las diferentes formas de Guardar datos
+var nombre = "JL-Gale con var";
+let nombreModerno = "JL-Gale con let";
+const nombreConstante = "JL-Gale con const"; 
+
+console.log(nombre);
+console.log(nombreModerno);
+console.log(nombreConstante);
+
+//Diferentes datos guardados
+let variableTexto = "Esto es una variable que guarda texto";
+let variableNumeroEntero = 12345;
+let variableDecimal = 12.234232;
+let variableBoleano = true;
+
+console.log(variableTexto);
+console.log(variableNumeroEntero);
+console.log(variableDecimal);
+console.log(variableBoleano);
+
+//Concatenacion de variabeles 
+let variableConcatenadas = variableTexto + " " + variableNumeroEntero + " " + variableDecimal + " " + variableBoleano;
+console.log(variableConcatenadas);
+
+
