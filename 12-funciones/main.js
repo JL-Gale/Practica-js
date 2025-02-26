ee5706f7ca47e08daf1cd46ce066e0811a047607
JL-Gale <jorge.galem@gmail.com>
2025-02-26 18:27:32 -0500

    Se practico el uso de funciones dentro de otras funciones

--- 12-funciones/main.js
+++ 12-funciones/main.js
@@ -30,3 +30,51 @@ function saludar4(nombre = "JL-Gale") {
   console.log(`Hola ${nombre} soy una funcion`);
 }
 saludar4();
+console.log(`***********`);
+
+//Funciones dentro de otras
+
+function operaciones(num1, num2) {
+  let resultados = {
+    suma: (num1 + num2),
+    resta: (num1 - num2),
+    multiplicacion: (num1 * num2),
+    division: (num1 / num2)
+  };
+  return resultados;
+}
+
+function mostrarPorConsola(num1, num2) {
+  let resultados = operaciones(num1, num2);
+  console.log(`num1: ${num1} num2: ${num2}`);
+  console.log(`Suma: ${resultados.suma}`);
+  console.log(`Resta: ${resultados.resta}`);
+  console.log(`Multiplicacion: ${resultados.multiplicacion}`);
+  console.log(`Division: ${resultados.division}`);
+  return true;
+}
+
+function mostrarPorPantalla(num1, num2) {
+    let resultados = operaciones(num1, num2);
+    document.write(`<h1>num1: ${num1} num2: ${num2}</h1>`);
+    document.write(`<h2>Resultados</h2>`);
+    document.write(`<h2>Suma: ${resultados.suma}</h2>`);
+    document.write(`<h2>Restas: ${resultados.resta}</h2>`);
+    document.write(`<h2>Multiplicacion: ${resultados.multiplicacion}</h2>`);
+    document.write(`<h2>Division: ${resultados.division}</h2>`);
+    document.write(`<hr>`);
+    return true;
+}
+
+function calculadora(num1, num2, mostrar = false) {
+  if (mostrar) {
+    mostrarPorConsola(num1, num2);
+  } else{
+    mostrarPorPantalla(num1, num2);
+  }
+  return true;
+}
+
+calculadora(10, 10, false);
+calculadora(10, 20, true);
+
